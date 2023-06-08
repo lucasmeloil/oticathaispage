d7e0d34ffc87257084e2258f83142d194744c9cf
viniccius-dev <santsmarcos81@gmail.com>
2023-06-08 19:40:19 -0300

    first commit

---
+++ js/article.js
@@ -0,0 +1,113 @@
+/* Caminhos para os botões */
+/* Lentes de Contato */
+
+var hash = window.location.hash;
+
+if (hash.length > 0) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.contact-lenses').classList.add('active');
+}
+
+document.querySelector('.btn-contact-lenses').addEventListener('click',function() {
+    window.location = "http://127.0.0.1:5500/article.html#id=contact-lenses";
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.contact-lenses').classList.add('active');
+});
+
+/* Exame de visão */
+
+document.querySelector('.btn-vision-exam').addEventListener('click', () => {
+    window.location = "http://127.0.0.1:5500/#id=vision-exam";
+});
+
+/* Menu Mobile */
+
+var btnMobile = document.querySelector('.menu-hamburguer');
+
+btnMobile.addEventListener('click', function() {
+    if(btnMobile.parentElement.className === 'menu-mobile') {
+        btnMobile.parentElement.className += ' activated';
+    } else {
+        btnMobile.parentElement.className =  'menu-mobile';
+    }
+});
+
+/* Menu Mobile - Óculos, lentes de contato e marque seu exame */
+
+var liGlass = document.querySelector('.li-glass');
+var liContactLenses = document.querySelector('.li-contact-lenses');
+var liVisionExam = document.querySelector('.li-vision-exam');
+
+liGlass.addEventListener('click',()=> {
+    window.location = "http://127.0.0.1:5500/article.html";
+});
+
+liContactLenses.addEventListener('click',()=> {
+    window.location = "http://127.0.0.1:5500/article.html#id=contact-lenses";
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.contact-lenses').classList.add('active');
+});
+
+liVisionExam.addEventListener('click',()=> {
+    window.location = "http://127.0.0.1:5500/#id=vision-exam";
+});
+
+/* Código para o filtro de preço */
+
+(function() {
+    const parent = document.querySelector('.range-slider');
+
+    if(!parent) {
+        return;
+    }
+
+    const rangeS = parent.querySelectorAll('input[type="range"]'),
+            numberS = parent.querySelectorAll('input[type="number"]');
+
+    rangeS.forEach((el) => {
+        el.oninput = () => {
+            let slide1 = parseFloat(rangeS[0].value),
+                slide2 = parseFloat(rangeS[1].value);
+
+            if(slide1 > slide2) {
+                [slide1, slide2] = [slide2, slide1];
+            }
+
+            numberS[0].value = slide1;
+            numberS[1].value = slide2;
+        }
+    });
+
+    numberS.forEach((el) => {
+        el.oninput = () => {
+            let number1 = parseFloat(numberS[0].value),
+                number2 = parseFloat(numberS[1].value);
+
+            if (number1 > number2) {
+                let tmp = number1;
+                numberS[0].value = number2;
+                numberS[1].value = tmp;
+            }
+
+            rangeS[0].value = number1;
+            rangeS[1].value = number2;
+        }
+    });
+}) ();
+
+/* Funcionalidade dos botões */
+
+var btnSection = document.querySelectorAll('.options-article h2');
+
+for(i = 0; i < btnSection.length; i++) {
+    btnSection[i].addEventListener('click',changeSection);
+}
+
+function changeSection(e) {
+
+    nameSection = e.target.id;
+
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.'+nameSection).classList.add('active');
+
+}
---
+++ js/main.js
@@ -0,0 +1,130 @@
+/* Caminhos para os botões */
+/* Lentes de Contato */
+
+document.querySelector('.btn-contact-lenses').addEventListener('click',function() {
+    window.location = "http://127.0.0.1:5500/article.html#id=contact-lenses";
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.contact-lenses').classList.add('active');
+});
+
+/* Exame de visão */
+/*** DEPOIS VERIRIFICAR ESSE CÓDIGO (AS VEZES FUNCIONA AS VEZES NÃO) ***/
+
+var hash = window.location.hash;
+var obj = document.querySelector('.vision-exam');
+var objPosition = obj.getBoundingClientRect().top;
+
+if (hash.length > 0) {
+    window.scrollBy(0,objPosition);
+}
+
+document.querySelector('.btn-vision-exam').addEventListener('click',(e) => {
+    e.preventDefault();
+    window.scrollBy(0,objPosition);
+})
+
+/* Menu Mobile */
+
+var btnMobile = document.querySelector('.menu-mobile');
+
+btnMobile.addEventListener('click', function() {
+    if(btnMobile.className === 'menu-mobile') {
+        btnMobile.className += ' activated';
+    } else {
+        btnMobile.className =  'menu-mobile';
+    }
+});
+
+/* Menu Mobile - Óculos, lentes de contato e marque seu exame */
+
+var liGlass = document.querySelector('.li-glass');
+var liContactLenses = document.querySelector('.li-contact-lenses');
+var liVisionExam = document.querySelector('.li-vision-exam');
+
+liGlass.addEventListener('click',()=> {
+    window.location = "http://127.0.0.1:5500/article.html";
+})
+
+liContactLenses.addEventListener('click',()=> {
+    window.location = "http://127.0.0.1:5500/article.html#id=contact-lenses";
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector('.contact-lenses').classList.add('active');
+})
+
+liVisionExam.addEventListener('click',(e)=> {
+    e.preventDefault();
+    window.scrollBy(0,objPosition);
+})
+
+/* Carrocel */
+
+function setImage() {
+
+    var settings = {
+
+        firstImage: function() {
+            element = document.querySelector('.slider div:first-child');
+            element.classList.add('active');
+        },
+
+        slide: function() {
+            element = document.querySelector('.active');
+            if(element.nextElementSibling){
+                element.nextElementSibling.classList.add('active');
+                element.classList.remove('active');
+            }else{
+                element.classList.remove('active');
+                settings.firstImage();
+            }
+        },
+
+        next: function() {
+            clearInterval(interval);
+            element = document.querySelector('.active');
+
+            if(element.nextElementSibling){
+                element.nextElementSibling.classList.add('active');
+                element.classList.remove('active');
+            }else{
+                element.classList.remove('active');
+                settings.firstImage();
+            }
+
+            interval = setInterval(settings.slide,4000);
+        },
+
+        prev: function() {
+            clearInterval(interval);
+            element = document.querySelector('.active');
+
+            if(element.previousElementSibling){
+                element.previousElementSibling.classList.add('active');
+                element.classList.remove('active');
+            }else{
+                element.classList.remove('active');
+                element = document.querySelector('.slider div:last-child');
+                element.classList.add('active');
+            }
+
+            interval = setInterval(settings.slide,4000);
+        }
+
+    }
+
+    //Chama o slide
+    settings.firstImage();
+
+    //Chama o próximo slide à um determinado tempo
+    var interval = setInterval(settings.slide,4000);
+    document.querySelector('.next').addEventListener('click',settings.next,false);
+    document.querySelector('.prev').addEventListener('click',settings.prev,false);
+}
+
+
+window.addEventListener('load',setImage,false);
+
+/* Botão para "ver mais" */
+
+btnViewMore = document.querySelector('.view-more');
+
+btnViewMore.addEventListener('click',()=>{ window.location =  "http://127.0.0.1:5500/article.html"});
